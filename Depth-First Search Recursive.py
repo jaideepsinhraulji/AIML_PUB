4f5ed2e747e013b43041f3a5fc8b51b442b2bf81
Jaideepsinh Raulji <92981558+jaideepsinhraulji@users.noreply.github.com>
2025-05-18 17:16:32 +0530

    Update Depth-First Search Recursive.py

--- Depth-First Search Recursive.py
+++ Depth-First Search Recursive.py
@@ -1,1 +1,20 @@
+graph = {
+  '6' : ['4','8'],
+  '4' : ['3', '5'],
+  '8' : ['9'],
+  '3' : [],
+  '5' : ['9'],
+  '9' : []
+}   #dictionary
 
+visited = []
+def dfs(visited, graph, node):
+    if node not in visited:
+        visited.append(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+##########################################
+if __name__=="__main__":
+    print("Depth-First Search")
+    dfs(visited, graph, '6')
+    print(visited)
