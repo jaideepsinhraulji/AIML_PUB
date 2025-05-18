8896f01f45291d333fbba0c20346afb90b306a20
Jaideepsinh Raulji <92981558+jaideepsinhraulji@users.noreply.github.com>
2025-05-18 17:14:44 +0530

    Update Depth-First Search.py

--- Depth-First Search.py
+++ Depth-First Search.py
@@ -1,1 +1,24 @@
+graph = {
+  '6' : ['4','8'],
+  '4' : ['3', '5'],
+  '8' : ['9'],
+  '3' : [],
+  '5' : ['9'],
+  '9' : []
+}   #dictionary
 
+
+visited = [] # List for visited nodes.
+stack = []     #Initialize a stack
+def dfs(visited, graph, node):
+  stack.insert(0,node)
+  while stack:          
+    m = stack.pop(0)
+    visited.append(m)
+    for child in graph[m]:  #accessing child or successors and adding them to stack
+      if child not in visited:
+         stack.insert(0,child)
+  print("Visited ",visited)#out of while loop
+# Driver Code
+print("Following is the Depth-First Search")
+dfs(visited, graph, '6')    # 6 is start state
